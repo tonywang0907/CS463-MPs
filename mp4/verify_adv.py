75722c8b66f7b29927ab659a9b64b619e68b8600
Tony <tony0907wang@gmail.com>
2024-11-09 19:07:59 -0800

    Checkpoint 3 attempt 1

--- mp4/verify_adv.py
+++ mp4/verify_adv.py
@@ -2,14 +2,54 @@ import numpy as np
 import json
 import pickle
 import os
+from mp4_model import SVM
 
 class Verifier:
     def __init__(self, jsonFile):
         self.jsonFile = jsonFile
         self.features = [0] * 5000
 
+    # def getFeatureArray(self, featureList, addedFeatureList):
+    #     with open(featureList, 'r') as f:
+    #         all_features = json.load(f)
+        
+    #     with open(addedFeatureList, 'r') as f:
+    #         added_features = f.readlines()
+
+
+    #     with open(self.jsonFile, 'r') as f:
+    #         original_features = json.load(f)
+        
+    #     feature_vector = [0] * len(all_features)
+
     def getFeatureArray(self, featureList, addedFeatureList):
-        pass
+        with open(featureList, 'r') as f:
+            all_features = json.load(f)  
+        
+        with open(addedFeatureList, 'r') as f:
+            added_features = f.readlines()  
+        
+        added_features_set = set([feature.strip() for feature in added_features])  
+        
+        with open(self.jsonFile, 'r') as f:
+            original_features = json.load(f)
+        
+        feature_name_to_index = {name: idx for idx, name in enumerate(all_features)}
+        
+        feature_vector = [0] * len(all_features)
+        
+        for feature in original_features:
+            if feature in feature_name_to_index:
+                idx = feature_name_to_index[feature]
+                feature_vector[idx] = 1
+        
+        for feature in added_features_set:
+            if feature in feature_name_to_index:
+                idx = feature_name_to_index[feature]
+                feature_vector[idx] = 1
+        
+        return np.array(feature_vector)  
+
 
 if __name__ == "__main__":
     verifier_5E06B7 = Verifier(
@@ -26,16 +66,25 @@ if __name__ == "__main__":
     aFList_49875A = "added-features-49875A.txt"
 
     # Merge the feature array with the added features
-    verifier_5E06B7.getFeatureArray(fList, aFList_5E06B7)
-    verifier_49875A.getFeatureArray(fList, aFList_49875A)
+    feature_vector_5E06B7 = verifier_5E06B7.getFeatureArray(fList, aFList_5E06B7)
+    feature_vector_49875A = verifier_49875A.getFeatureArray(fList, aFList_49875A)
     # Load trained SVM model with 5000 features
 
+    model_filename = "svm-f5000.p"
+    with open(model_filename, 'rb') as f:
+        svm_model = pickle.load(f)
+
     # Make predictions
+    prediction_5E06B7 = svm_model.clf.predict([feature_vector_5E06B7])
+    prediction_49875A = svm_model.clf.predict([feature_vector_49875A])
 
     # Output predicted label and original label for each sample
+    
+    print('{"5E06B7": ' + str(prediction_5E06B7[0]) + ', "49875A": ' + str(prediction_49875A[0]) + '}')
     # Object needed to be printed out -  res = {"5E06B7": <value>, "49875A": <value>}
     # The predicted value can be 0 or 1. 
     # Expected output
     # {"5E06B7": 1, "49875A": 0}
     
     # Please put your thought as code comments below why these features might be helpful to make an adversarial sample.
+
